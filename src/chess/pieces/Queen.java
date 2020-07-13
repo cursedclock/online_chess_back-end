a1ad89819cb9e665a844d9ed0538f46bc1a94ed0
mostafa <mos01naj@gmail.com>
2020-07-13 08:14:54 +0430

    Added queen.

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,14 @@
+package chess.pieces;
+
+import chess.Position;
+
+public class Queen extends Piece{
+    public Queen(Colour colour){
+        super(colour);
+    }
+
+    @Override
+    public void verifyMove(Position p1, Position p2) {
+
+    }
+}
